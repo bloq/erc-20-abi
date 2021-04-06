c75f43f505f0d11a683c2d450485044e117090b8
Gabriel Montes <gabriel@bloq.com>
2021-04-06 16:01:30 -0300

    Add JSDocs for simpler-contract

--- packages/web3-simpler-contract/README.md
+++ packages/web3-simpler-contract/README.md
@@ -1,12 +1,12 @@
 # web3-simpler-contract
 
-For scenarios like simple dApps or CLI tools, it is sometimes easier to interact with a subset or a simplified version of the Web3.js contract interface. This package creates such a simpler version of a contract object with a property for each contract method that returns a Promise.
+For scenarios like simple dApps or CLI tools, it is sometimes easier to interact with a subset or a simplified version of the [Web3.js](https://github.com/ChainSafe/web3.js) contract interface. This package creates such a simpler version of a contract object with a property for each contract method that returns a `Promise`.
 
 So calls to view methods just follow the pattern `contract.method(...args)` as well as calls to methods that will end up signing and sending a transaction.
 
 As an advantage over the Web3.js contract interface, and to simplify the user code, when sending a transaction and the gas is not specified, the simpler contract will do the gas estimation automatically.
 
-Note that given this creates a simplified contract, many Web3.js contract features are not supported
+Note that given this creates a simplified contract, many Web3.js contract features are not supported and this is intentional.
 
 ## Install
 
@@ -30,6 +30,10 @@ simplerToken.balanceOf(myAddress).then(console.log) // prints my balance
 
 simplerToken.approve(spenderAddress, amount).then(console.log) // prints the transaction receipt
 ```
+
+## API
+
+See the [API documentation](API.md).
 
 ## License
 
--- packages/web3-simpler-contract/src/index.js
+++ packages/web3-simpler-contract/src/index.js
@@ -3,16 +3,83 @@
 const debug = require('debug')('web3-simpler-contract')
 
 /**
+ * @module web3-simpler-contract
+ */
+
+/**
+ * @ignore
+ * @typedef {import('web3').default} Web3
+ * @see https://github.com/ChainSafe/web3.js/tree/1.x/packages/web3
+ */
+
+/**
+ * @ignore
+ * @typedef {import('web3-utils').AbiItem} AbiItem
+ * @see https://github.com/ChainSafe/web3.js/tree/1.x/packages/web3-utils
+ */
+
+/* eslint-disable jsdoc/check-types */
+/**
+ * The contract address for each chain. It is an object that maps a chain ID to
+ * the address of the contract in that chain.
+ *
+ * @typedef {Object.<number,string>} ContractAddresses
+ */
+/* eslint-enable jsdoc/check-types */
+
+/**
+ * The options for the simpler contract. These are the Web3 contract options
+ * with the addition of `gasFactor`.
+ *
+ * @typedef {object} ContractOptions
+ * @property {string} [data] See Web3 contract options.
+ * @property {string} [from] See Web3 contract options.
+ * @property {number} [gas] See Web3 contract options.
+ * @property {number} [gasFactor] The gas over-estimation factor.
+ * @property {string} [gasPrice] See Web3 contract options.
+ */
+
+/**
+ * Calls a contract method. It receives a variable number of parameters where
+ * the first N are the actual parameters of the contract method. The last 2 are
+ * optional: the call options and the default block.
+ *
+ * @typedef {Function} CallMethod
+ * @param {...*} args The method params, call options and default block.
+ * @returns {Promise} The return value of the call.
+ */
+
+/**
+ * Sends a transaction by executing a contract method. It receives a variable
+ * number of parameters where the first N are the actual parameters of the
+ * contract method. The last is optional: the send options.
+ *
+ * @typedef {Function} SendMethod
+ * @param {...*} args The method params and send options.
+ * @returns {Promise<Web3.Eth.TransactionReceipt>} The transaction receipt.
+ */
+
+/* eslint-disable jsdoc/check-types */
+/**
+ * The simpler contract object. It has properties for each contract method that
+ * can be "called" or "sent".
+ *
+ * @typedef {Object.<string,CallMethod|SendMethod>} SimplerContract
+ */
+/* eslint-enable jsdoc/check-types */
+
+/**
  * Creates an object with a simplified interface to interact with Web3.js
  * contracts.
  *
- * @param {object} web3 A Web3.js instance.
- * @param {object[]} abi The JSON interface or ABI of the contract.
- * @param {object} addresses The addresses of the contract per chain.
- * @param {object} [options] Additional properties for the Web3.js contract.
- * @returns {object} The simpler contract object.
+ * @param {Web3} web3 A Web3 instance.
+ * @param {AbiItem[]} abi The ABI of the contract.
+ * @param {ContractAddresses} addresses The address of the contract on each chain.
+ * @param {ContractOptions} [options] More options for the Web3.js contract.
+ * @returns {SimplerContract} The simpler contract object.
+ * @alias module:web3-simpler-contract
  */
-const createWeb3SimplerContract = function (web3, abi, addresses, options) {
+function createWeb3SimplerContract(web3, abi, addresses, options = {}) {
   debug('Creating simpler contract')
 
   // Get the contract address based on the chain ID and create the contract.
@@ -28,7 +95,7 @@ const createWeb3SimplerContract = function (web3, abi, addresses, options) {
   const safeGas = ({ gasFactor = 2 }) => (gas) => Math.ceil(gas * gasFactor)
 
   // Generic method.call() wrapper.
-  const createCall = ({ inputs, name }) =>
+  const createCall = ({ inputs = [], name = 'fallback' }) =>
     function (...args) {
       debug('Calling %s', name)
 
@@ -55,7 +122,7 @@ const createWeb3SimplerContract = function (web3, abi, addresses, options) {
     }
 
   // Generic method.send() wrapper. It also estimates the gas if needed.
-  const createSend = ({ inputs, name }) =>
+  const createSend = ({ inputs = [], name = 'fallback' }) =>
     function (...args) {
       debug('calling %s', name)
 
@@ -68,7 +135,7 @@ const createWeb3SimplerContract = function (web3, abi, addresses, options) {
 
         return Promise.resolve(
           sendOptions.gas ||
-            method.estimateGas({ ...sendOptions }).then(safeGas(sendOptions))
+            method.estimateGas({ ...sendOptions }).then(safeGas(options))
         ).then(function (gas) {
           const promiEvent = method.send({ ...sendOptions, gas })
 
@@ -91,7 +158,7 @@ const createWeb3SimplerContract = function (web3, abi, addresses, options) {
 
   // Create all the contract simpler methods based on the ABI.
   const methods = abi
-    .filter((desc) => desc.type === 'function')
+    .filter((desc) => desc.type === 'function' && desc.name)
     .map(function (desc) {
       const { name, stateMutability } = desc
       switch (stateMutability) {
@@ -99,6 +166,8 @@ const createWeb3SimplerContract = function (web3, abi, addresses, options) {
           return { name, fn: createCall(desc) }
         case 'nonpayable':
           return { name, fn: createSend(desc) }
+        default:
+          throw new Error(`Unsupported mutability type: ${stateMutability}`)
       }
     })
     .reduce((all, { name, fn }) => Object.assign(all, { [name]: fn }), {})
